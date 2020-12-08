26c7d954fa44ed5a28c3504ab230ad134fc42576
Radhika Agrawal <agrawalr@usc.edu>
2020-12-08 13:34:08 -0500

    added pubsub from webpage to pi

--- driver.py
+++ driver.py
@@ -1,36 +1,48 @@
 from pubnub.callbacks import SubscribeCallback
-from pubnub.enums import PNStatusCategory
+from pubnub.enums import PNStatusCategory, PNOperationType
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
-from pprint import pprint
-
-class MySubscribeCallback(SubscribeCallback):
-    def status(self, pubnub, status):
-        pass
-
-    def presence(self, pubnub, presence):
-        pprint(presence.__dict__)
-
-    def message(self, pubnub, message):
-        pprint(message.__dict__)
-
-def my_publish_callback(envelope, status):
-    print(envelope, status)
 
 pnconfig = PNConfiguration()
 pnconfig.subscribe_key = "sub-c-d6f1853a-38ed-11eb-99ef-fa1b309c1f97"
 pnconfig.publish_key = "pub-c-63771edf-cfc7-4d18-b066-24b1e7f0e40c"
-
 pubnub = PubNub(pnconfig)
 
-pubnub.add_listener(MySubscribeCallback())
+def my_publish_callback(envelope, status):
+    # Check whether request successfully completed or not
+    if not status.is_error():
+        pass  # Message successfully published to specified channel.
+    else:
+        print("error")
 
-pubnub.subscribe()\
-    .channels("pubnub_onboarding_channel")\
-    .with_presence()\
-    .execute()\
+class LightUpCallback(SubscribeCallback):
+    def presence(self, pubnub, presence):
+        pass  # handle incoming presence data
 
-pubnub.publish()\
-    .channel("pubnub_onboarding_channel")\
-    .message({"sender": pnconfig.uuid, "content": "Hello From Python SDK"})\
-    .pn_async(my_publish_callback)
+    def status(self, pubnub, status):
+        if status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+            pass  # This event happens when radio / connectivity is lost
+
+        elif status.category == PNStatusCategory.PNConnectedCategory:
+            # Connect event. You can do stuff like publish, and know you'll get it.
+            # Or just use the connected event to confirm you are subscribed for
+            # UI / internal notifications, etc
+            pubnub.publish().channel('raspberry-control').message('Hello channel!').pn_async(my_publish_callback)
+        elif status.category == PNStatusCategory.PNReconnectedCategory:
+            pass
+            # Happens as part of our regular operation. This event happens when
+            # radio / connectivity is lost, then regained.
+        elif status.category == PNStatusCategory.PNDecryptionErrorCategory:
+            pass
+            # Handle message decryption error. Probably client configured to
+            # encrypt messages and on live data feed it received plain text.
+
+    def message(self, pubnub, message):
+        # Handle new message stored in message.message
+        print('message is: ' + str(message.message))
+        if(message.message == 'pink'):
+            state = 'lit'
+            pubnub.publish().channel('raspberry-control').message(state).pn_async(my_publish_callback)
+
+pubnub.add_listener(LightUpCallback())
+pubnub.subscribe().channels('raspberry-control').execute()
